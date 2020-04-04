499b2b615f1bd3d2dbbafcf0f7d14a418b2b35b9
Armando89 <43739205+Armando89@users.noreply.github.com>
2020-04-04 03:02:16 -0500

    Update
    Added the Commission method

--- src/assignment5c/Assignment5C.java
+++ src/assignment5c/Assignment5C.java
@@ -10,34 +10,28 @@ public class Assignment5C
 {
     public static void main(String[] args) 
     {
-        //final double BASE_PAY = 200;
-        //final double COMMISSION = .10;
-        //double wPaycheck, hrRate, otPay, regularPay, priceItems,
-        //        totalAmount, totalComm;
-        //int hrs, otHours, soldItems;
         int answer;
         String fName, lName, fullName, salEmployee, hrEmployee, commEmployee;
         
         JOptionPane.showMessageDialog(null, "Welcome to the HR Payroll Program.");
         answer = Integer.parseInt(JOptionPane.showInputDialog("Please provide "
-            + "the following information: If the employee is salary enter a 1, if "
-            + "the employee is hourly enter a 2, if the employee is commission "
+            + "the following information: If the employee is salary enter a 1, "
+            + "if the employee is hourly enter a 2, if the employee is commission "
             + "enter a 3, or to exit the program enter a 4."));
         
             while (answer < 1 || answer > 4)
             {
-                answer = Integer.parseInt(JOptionPane.showInputDialog("Invalid input: "
-                    + "Please provide a number between 1 and 4."));
+                answer = Integer.parseInt(JOptionPane.showInputDialog("Invalid "
+                    + "input: Please provide a number between 1 and 4."));
             }   
         
         fName = JOptionPane.showInputDialog("Please enter your first name:");
         lName = JOptionPane.showInputDialog("Please enter your last name:");
-        //fullName = Name(fName, lName);
         fullName = fName + lName;
     
         salEmployee = Salary(fullName);
         hrEmployee = Hourly(fullName);
-        //commEmployee = Commission(fullName);
+        commEmployee = Commission(fullName);
  
         /*
         Receive an integer of 1 back from each method such that counters will tally the number of weekly paychecks calculated for each type of employee.
@@ -45,23 +39,18 @@ public class Assignment5C
         */
     }
 
-    /*public static String Name(String firstName, String lastName)
-    {
-        return firstName + lastName;
-    }*/
-    
     public static String Salary(String Name)
     {
         double yrSal, annBonus, wPaycheck;
         String message;
         
-        yrSal = Double.parseDouble(JOptionPane.showInputDialog("Please "
-            + "provide the employee's yearly salary:"));
+        yrSal = Double.parseDouble(JOptionPane.showInputDialog("Please provide "
+            + "the employee's yearly salary:"));
 
-            while (yrSal < 30000 || yrSal > 65000)
+            while (yrSal < 35000 || yrSal > 65000)
             {
-                yrSal = Double.parseDouble(JOptionPane.showInputDialog("Invalid input: "
-                    + "The amount must be between $30,000 and $65,000."));
+                yrSal = Double.parseDouble(JOptionPane.showInputDialog("Invalid "
+                    + "input: The amount must be between $35,000 and $65,000."));
             }
         
         annBonus = Double.parseDouble(JOptionPane.showInputDialog("Please provide "
@@ -91,10 +80,10 @@ public class Assignment5C
         hrRate = Double.parseDouble(JOptionPane.showInputDialog("Please provide "
             + "the employee's hourly rate:"));
                         
-            while (hrRate < 10 || hrRate > 25)
+            while (hrRate < 15 || hrRate > 25)
                 {
                     hrRate = Double.parseDouble(JOptionPane.showInputDialog("Invalid "
-                        + "input: The amount must be between $10.00 and $25.00."));
+                        + "input: The amount must be between $15.00 and $25.00."));
                 }
                     
         hrs = Integer.parseInt(JOptionPane.showInputDialog("Please provide the "
@@ -141,11 +130,41 @@ public class Assignment5C
     */
     }
     
+    public static String Commission(String Name)
+    {
+        final double COMMISSION = .10;
+        final double BASE_PAY = 200;
+        double priceItems, totalAmount, totalComm, wPaycheck;
+        int soldItems;
+        String message;
+        
+        soldItems = Integer.parseInt(JOptionPane.showInputDialog("Please enter "
+            + "the number of items sold:"));
+                    
+            while (soldItems < 0 || soldItems > 500)
+            {
+                soldItems = Integer.parseInt(JOptionPane.showInputDialog("Invalid "
+                    + "input: The number of items sold must be a positive number "
+                    + "and should not exceed the quantity of 500."));
+            }
+                        
+        priceItems = Double.parseDouble(JOptionPane.showInputDialog("Please enter "
+            + "the price of the item(s):"));
+                    
+        totalAmount = (double)soldItems * priceItems;
+        totalComm = totalAmount * COMMISSION;
+        wPaycheck = totalComm + BASE_PAY;
+                    
+        message = String.format("This summary is for employee: %s. The total "
+            + "amount for the items sold is $%.2f. The employee's commission "
+            + "amount is $%.2f and its total paycheck amount equals $%.2f.", Name, 
+            totalAmount, totalComm, wPaycheck);
+        JOptionPane.showMessageDialog(null, message);
     
+        return message;
     
-        
-        
-        
-        
-  
+    /*
+    Pass back an integer of “1” directly to get added to the counter for the number of commission employees whose paycheck has been calculated
+    */
+    }
 }
