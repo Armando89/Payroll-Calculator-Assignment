cca9f4e11d20077c96c2ea4042f33723bfaeb53d
Armando89 <43739205+Armando89@users.noreply.github.com>
2020-04-04 04:07:55 -0500

    Update
    Modified the main method

--- src/assignment5c/Assignment5C.java
+++ src/assignment5c/Assignment5C.java
@@ -14,38 +14,64 @@ public class Assignment5C
         String fName, lName, fullName, salEmployee, hrEmployee, commEmployee;
         
         JOptionPane.showMessageDialog(null, "Welcome to the HR Payroll Program.");
-        answer = Integer.parseInt(JOptionPane.showInputDialog("Please provide "
-            + "the following information: If the employee is salary enter a 1, "
-            + "if the employee is hourly enter a 2, if the employee is commission "
-            + "enter a 3, or to exit the program enter a 4."));
         
-            while (answer < 1 || answer > 4)
+        do
+        {
+            answer = Integer.parseInt(JOptionPane.showInputDialog("Please provide "
+                + "the following information: If the employee is salary enter a 1, "
+                + "if the employee is hourly enter a 2, if the employee is commission "
+                + "enter a 3, or to exit the program enter a 4."));
+
+                while (answer < 1 || answer > 4)
+                {
+                    answer = Integer.parseInt(JOptionPane.showInputDialog("Invalid "
+                        + "input: Please provide a number between 1 and 4."));
+                }
+            
+            switch (answer)
             {
-                answer = Integer.parseInt(JOptionPane.showInputDialog("Invalid "
-                    + "input: Please provide a number between 1 and 4."));
-            }   
+                case 1:
+                    fName = JOptionPane.showInputDialog("Please enter your first "
+                        + "name:");
+                    lName = JOptionPane.showInputDialog("Please enter your last "
+                        + "name:");
+            
+                    salEmployee = Salary(fName, lName);
+                    break;
+                    
+                case 2:
+                    fName = JOptionPane.showInputDialog("Please enter your first "
+                        + "name:");
+                    lName = JOptionPane.showInputDialog("Please enter your last "
+                        + "name:");
+            
+                    hrEmployee = Hourly(fName, lName);
+                    break;
+                    
+                case 3:
+                    fName = JOptionPane.showInputDialog("Please enter your first "
+                        + "name:");
+                    lName = JOptionPane.showInputDialog("Please enter your last "
+                        + "name:");
+            
+                    commEmployee = Commission(fName, lName);
+                    break;
+            }
+        } while (answer != 4);
         
-        fName = JOptionPane.showInputDialog("Please enter your first name:");
-        lName = JOptionPane.showInputDialog("Please enter your last name:");
-        fullName = fName + lName;
-    
-        salEmployee = Salary(fullName);
-        hrEmployee = Hourly(fullName);
-        commEmployee = Commission(fullName);
- 
         /*
         Receive an integer of 1 back from each method such that counters will tally the number of weekly paychecks calculated for each type of employee.
         Print out the number of total successful paychecks calculated for each type of employee.
         */
     }
 
-    public static String Salary(String Name)
+    public static String Salary(String firstName, String lastName)
     {
         double yrSal, annBonus, wPaycheck;
         String message;
         
         yrSal = Double.parseDouble(JOptionPane.showInputDialog("Please provide "
-            + "the employee's yearly salary:"));
+            + "your yearly salary:"));
 
             while (yrSal < 35000 || yrSal > 65000)
             {
@@ -54,14 +80,14 @@ public class Assignment5C
             }
         
         annBonus = Double.parseDouble(JOptionPane.showInputDialog("Please provide "
-            + "the annual profit bonus percent:"));
+            + "your company's annual profit bonus percent:"));
         
         wPaycheck = (yrSal * (1 + annBonus)) / 52;
         
-        message = String.format("This summary is for employee: %s. The employee's "
+        message = String.format("This summary is for employee: %s %s. The employee's "
             + "yearly salary is $%.2f, which doesn't include the annual profit "
-            + "bonus of %.2f percent, and its weekly paycheck amount is $%.2f.", 
-            Name, yrSal, annBonus, wPaycheck);
+            + "bonus of %.2f percent, and the weekly paycheck amount is $%.2f.", 
+            firstName, lastName, yrSal, annBonus, wPaycheck);
         JOptionPane.showMessageDialog(null, message);
         
         return message;
@@ -71,14 +97,14 @@ public class Assignment5C
     */  
     }
     
-    public static String Hourly(String Name)
+    public static String Hourly(String firstName, String lastName)
     {
         int hrs, otHours;
         double hrRate, otPay, regularPay, wPaycheck;
         String message;
         
         hrRate = Double.parseDouble(JOptionPane.showInputDialog("Please provide "
-            + "the employee's hourly rate:"));
+            + "your hourly rate:"));
                         
             while (hrRate < 15 || hrRate > 25)
                 {
@@ -104,10 +130,11 @@ public class Assignment5C
                 regularPay = hrRate * 40;
                 wPaycheck = regularPay + otPay;
 
-                message = String.format("This summary is for employee: %s. The "
+                message = String.format("This summary is for employee: %s %s. The "
                     + "employee's regular pay amount is $%.2f and its overtime "
                     + "pay amount is $%.2f. Overall, the employee's weekly paycheck "
-                    + "amount equals to $%.2f.", Name, regularPay, otPay, wPaycheck);
+                    + "amount equals to $%.2f.", firstName, lastName, regularPay, 
+                    otPay, wPaycheck);
                 JOptionPane.showMessageDialog(null, message);
             }
             else
@@ -116,10 +143,10 @@ public class Assignment5C
                 regularPay = hrRate * hrs;
                 wPaycheck = regularPay + otPay;
 
-                message = String.format("This summary is for employee: %s. Since "
-                    + "the employee did not work any overtime, its overtime pay "
-                    + "equals $%.2f and its weekly paycheck/regular pay amount is "
-                    + "$%.2f.", Name, otPay, wPaycheck);
+                message = String.format("This summary is for employee: %s %s. "
+                    + "Since the employee did not work any overtime, the overtime "
+                    + "pay is $%.2f and the weekly paycheck/regular pay amount "
+                    + "equals to $%.2f.", firstName, lastName, otPay, wPaycheck);
                 JOptionPane.showMessageDialog(null, message);
             }
         
@@ -130,7 +157,7 @@ public class Assignment5C
     */
     }
     
-    public static String Commission(String Name)
+    public static String Commission(String firstName, String lastName)
     {
         final double COMMISSION = .10;
         final double BASE_PAY = 200;
@@ -155,10 +182,10 @@ public class Assignment5C
         totalComm = totalAmount * COMMISSION;
         wPaycheck = totalComm + BASE_PAY;
                     
-        message = String.format("This summary is for employee: %s. The total "
-            + "amount for the items sold is $%.2f. The employee's commission "
-            + "amount is $%.2f and its total paycheck amount equals $%.2f.", Name, 
-            totalAmount, totalComm, wPaycheck);
+        message = String.format("This summary is for employee: %s %s. The total "
+            + "amount for the item(s) sold is $%.2f. The employee's commission "
+            + "amount equals to $%.2f and the total paycheck amount comes to $%.2f.", 
+            firstName, lastName, totalAmount, totalComm, wPaycheck);
         JOptionPane.showMessageDialog(null, message);
     
         return message;
